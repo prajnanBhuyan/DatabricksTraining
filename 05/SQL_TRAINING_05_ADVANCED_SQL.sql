b7bae7b80e62e858b008fb3c0258b5f9e4e796d9
Prajnan Bhuyan <prajnan.bhuyan@milliman.com>
2023-09-14 09:55:37 +0530

    Added analytic window functions

--- 05/SQL_TRAINING_05_ADVANCED_SQL.sql
+++ 05/SQL_TRAINING_05_ADVANCED_SQL.sql
@@ -46,6 +46,9 @@ CREATE DATABASE IF NOT EXISTS $TRAINING_DATABASE;
 -- MAGIC
 -- MAGIC def check_exercise1_solution():
 -- MAGIC   check_solution(1, 1493914765122705214)
+-- MAGIC
+-- MAGIC def check_exercise2_solution():
+-- MAGIC   check_solution(2, 6108592852707132683)
 
 -- COMMAND ----------
 
@@ -247,21 +250,28 @@ ORDER BY MONTH(PICKUP_DATETIME), TRIP_DISTANCE_RANK
 
 -- DBTITLE 1,Problem
 -- MAGIC %md
--- MAGIC ***Busiest Hours for Taxi Trips***<br>
+-- MAGIC ***Busiest Hours for Taxi Trips***
+-- MAGIC
+-- MAGIC In this exercise, you will explore and analyze the busiest hours of the day for taxi trips in our dataset. You will rank the hours based on the number of taxi trips that occur during each hour, allowing you to identify the most and least active times for taxi rides.
+-- MAGIC
+-- MAGIC **Problem Statement:**
+-- MAGIC
 -- MAGIC - Rank the hours of the day (pickup_datetime) based on the number of taxi trips that occur during each hour.
--- MAGIC - Determine the 2nd and the 5th most busiest hours for taxi rides.
--- MAGIC - In case or a draw/tie select the hour in chronological order
+-- MAGIC - Determine the 2nd and the 5th busiest hours for taxi rides.
+-- MAGIC - In case of a draw or tie in trip counts, select the hours in chronological order.
 -- MAGIC - Your solution view should have three columns:
--- MAGIC   - HOUR_OF_DAY: Self-explanatory, the hour of the day
--- MAGIC   - TRIPS: Number of trips that occur at that hour
--- MAGIC   - RANK: Rank of how busy taxis are at that hour, the higher the rank the less busy it is
--- MAGIC - Your solution should have two records, one for the 2nd bussiest and one for the 5th
+-- MAGIC   - HOUR_OF_DAY: This column represents the hour of the day when trips occur.
+-- MAGIC   - TRIPS: The number of taxi trips that take place during that hour.
+-- MAGIC   - RANK: The rank of how busy taxis are during that hour, with a higher rank indicating less busyness.
+-- MAGIC - Your solution should include only two records: one for the 2nd busiest hour and another for the 5th busiest hour.
 -- MAGIC - Sample EXERCISE1_SOLUTION view:
 -- MAGIC   | HOUR_OF_DAY | TRIPS | RANK |
 -- MAGIC   |-------------|-------|------|
 -- MAGIC   | 7           | 12372 | 2    |
 -- MAGIC   | 23          | 34634 | 5    |
--- MAGIC - Expected schema:<br>
+-- MAGIC
+-- MAGIC **Expected Schema:**
+-- MAGIC - The expected schema for your solution view should be as follows:<br>
 -- MAGIC   root<br>
 -- MAGIC   &nbsp;&nbsp;|-- HOUR_OF_DAY: integer (nullable = true)<br>
 -- MAGIC   &nbsp;&nbsp;|-- TRIPS: long (nullable = false)<br>
@@ -271,14 +281,122 @@ ORDER BY MONTH(PICKUP_DATETIME), TRIP_DISTANCE_RANK
 
 -- DBTITLE 1,Solution
 CREATE OR REPLACE TEMP VIEW EXERCISE1_SOLUTION AS
-SELECT
-  /* AND ADD YOUR CODE HERE */
+  /* ADD YOUR CODE HERE */
 
 -- COMMAND ----------
 
 -- DBTITLE 1,Check Solution
 -- MAGIC %python
 -- MAGIC check_exercise1_solution()
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC #### Analytic window functions
+-- MAGIC
+-- MAGIC Analytic window functions perform calculations across a set of rows related to the current row, but they do not assign a rank. Instead, they provide aggregated or computed values based on a window of rows.
+-- MAGIC <br>
+-- MAGIC https://learn.microsoft.com/en-us/azure/databricks/sql/language-manual/sql-ref-functions-builtin#analytic-window-functions
+-- MAGIC
+-- MAGIC How are analytic window functions useful in terms of our dataset? Here are some obvious things we can do:
+-- MAGIC - Running Total of Fare Amount:
+-- MAGIC   Calculate the running total of the fare amount for each vendor (VENDOR_ID) over time. Analyze how the total earnings change as more trips are completed by each vendor.
+-- MAGIC
+-- MAGIC - Identifying Trips with the Highest Total Amount by Day:
+-- MAGIC   Calculate the total amount (TOTAL_AMOUNT) for each trip and find the trip with the highest total amount for each day. Analyze daily outliers in trip earnings.
+-- MAGIC
+-- MAGIC - Calculating Trip Duration Percentile Rank:
+-- MAGIC   Calculate the percentile rank of each trip's duration (DROPOFF_DATETIME - PICKUP_DATETIME) within the dataset to identify trips that fall into specific duration percentiles.
+-- MAGIC
+-- MAGIC - Finding the Most Popular Pickup Locations by Hour:
+-- MAGIC   Rank pickup locations (PICKUP_LONGITUDE and PICKUP_LATITUDE) by their popularity within each hour of the  day. Identify the most frequently used pickup locations for each time slot.
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Simple Example
+SELECT
+  COL1,
+  COL2,
+  LAG(COL2) OVER (PARTITION BY COL1 ORDER BY COL2) AS LAG,
+  LEAD(COL2) OVER (PARTITION BY COL1 ORDER BY COL2) AS LEAD,
+  CUME_DIST() OVER (PARTITION BY COL1 ORDER BY COL2) AS CUME_DIST
+FROM VALUES
+  ('A', 1), ('A', 2), ('A', 3), ('A', 3), ('A', 5),
+  ('B', 1), ('B', 2), ('B', 2), ('B', 5), ('B', 5), ('B', 6), ('B', 7),
+  ('C', 1), ('C', 2), ('C', 3)
+TAB(COL1, COL2);
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Running Total of Fare Amount
+-- Since our dataset is realtively large, to make this
+-- query faster we will use just the first 100 records
+-- of the dataset. Feel free to play around with this
+WITH CLEANED_NYCTAXI_YELLOW_SUBSET AS (
+  SELECT
+    *
+  FROM $TRAINING_DATABASE.CLEANED_NYCTAXI_YELLOW
+  LIMIT 100
+)
+SELECT
+  VENDOR_ID,
+  DATE_FORMAT(PICKUP_DATETIME, 'yyyy-MM-dd HH:mm:ss') AS PICKUP_DATETIME,
+  FARE_AMOUNT,
+  SUM(FARE_AMOUNT) OVER(
+    PARTITION BY VENDOR_ID
+    ORDER BY PICKUP_DATETIME
+  ) AS RUNNING_TOTAL_FARE,
+
+  '=========' AS SEPARATOR,  
+  *
+FROM CLEANED_NYCTAXI_YELLOW_SUBSET
+ORDER BY
+  VENDOR_ID, PICKUP_DATETIME
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ##### Exercise-2
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ***Identifying Anomalies in Trip Counts***
+-- MAGIC
+-- MAGIC In this exercise, you will be tasked with identifying anomalies in the number of taxi trips taken each day. An anomaly refers to a day where the number of trips significantly deviates from the expected or average count. This analysis can help detect unusual or irregular patterns in taxi usage.
+-- MAGIC
+-- MAGIC **Problem Statement:**
+-- MAGIC
+-- MAGIC - Calculate the daily trip count for ***each day for the month of December*** in the dataset.
+-- MAGIC - Calculate a rolling average (e.g., 7-day moving average) of daily trip counts to establish a baseline trend.
+-- MAGIC - Identify and flag days where the actual daily trip count significantly differs from the rolling average.
+-- MAGIC - For this test, we will ***flag days where the trip count is 2 standard deviations away*** from the rolling average.
+-- MAGIC - Your solution view should have three columns:
+-- MAGIC   - DATE: The date of the trip count.
+-- MAGIC   - DAILY_TRIP_COUNT: The number of trips taken on that day.
+-- MAGIC   - IS_ANOMALY: A binary indicator (1 or 0) representing whether the day is an anomaly or not.
+-- MAGIC - Your solution should list the dates of the identified anomalies along with their daily trip counts and the corresponding anomaly flag.
+-- MAGIC - Sample EXERCISE2_SOLUTION view:
+-- MAGIC   | DATE       | DAILY_TRIP_COUNT | IS_ANOMALY |
+-- MAGIC   |------------|-------------------|------------|
+-- MAGIC   | 2023-01-01 | 1500              | 0          |
+-- MAGIC   | 2023-01-02 | 2000              | 1          |
+-- MAGIC   | 2023-01-03 | 1700              | 0          |
+-- MAGIC - Expected schema:<br>
+-- MAGIC   root<br>
+-- MAGIC   &nbsp;&nbsp;|-- DATE: date (nullable = true)<br>
+-- MAGIC   &nbsp;&nbsp;|-- DAILY_TRIP_COUNT: long (nullable = false)<br>
+-- MAGIC   &nbsp;&nbsp;|-- IS_ANOMALY: integer (nullable = false)
+
+-- COMMAND ----------
+
+CREATE OR REPLACE TEMP VIEW EXERCISE2_SOLUTION AS
+  /* ADD YOUR CODE HERE */
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC check_exercise2_solution()
 
 -- COMMAND ----------
 
